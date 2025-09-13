8f6f2e7138976e592a12e0024e9252430caac7e7
Msandeep <msandeep96693@gmail.com>
2025-09-13 18:21:18 +0530

    page class added

---
+++ src/test/java/pageobject/createlistingpage.java
@@ -0,0 +1,15 @@
+package pageobject;
+
+import org.openqa.selenium.WebDriver;
+
+public class createlistingpage extends Basicpage {
+	
+	public createlistingpage(WebDriver driver)
+	{
+		super(driver);
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/pageobject/listingdashboardpage.java
@@ -0,0 +1,11 @@
+package pageobject;
+
+import org.openqa.selenium.WebDriver;
+
+public class listingdashboardpage extends Basicpage {
+
+	public listingdashboardpage(WebDriver driver)
+	{
+		super(driver);
+	}
+}
